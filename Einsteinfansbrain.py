8b3c5973659dac8a168b628bc2c25f43bcb160e7
MirellaStone <marianagia.santanach@gmail.com>
2025-01-20 20:24:14 -0500

    Create Einsteinfansbrain.py
    Es un bot de discord que hace muchas cosas :)

---
+++ Einsteinfansbrain.py
@@ -0,0 +1,29 @@
+import discord
+from Safe_key import *
+
+# La variable intenta almacenar los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("bye!")
+    elif message.content.startswith("password"):
+        await message.channel.send(llave(10))
+
+    else:
+        await message.channel.send(message.content)
+
+client.run("TOKEN")
